03402ac9b1537d919fc52cfd3e6196a4733a5e71
Eric <ejh81491@gmail.com>
2015-12-10 13:54:36 -0500

    fix transition

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(function(){
+
+
+
+$(".sect1").on('mouseover', function(){
+	$(".table1").css({top:30, left: 208, position:'absolute'});
+})
+$(".sect1").on('mouseout', function(){
+	$(".table1").css({top:48, left: 208, position:'absolute'});
+})
+
+
+
+$(".sect2").on('mouseover', function(){
+	$(".table2").css({top:147, left: 208, position:'absolute'});
+})
+$(".sect2").on('mouseout', function(){
+	$(".table2").css({top:165, left: 208, position:'absolute'});
+})
+
+
+
+$(".sect3").on('mouseover', function(){
+	$(".table3").css({top:267, left: 208, position:'absolute'});
+})
+$(".sect3").on('mouseout', function(){
+	$(".table3").css({top:285, left: 208, position:'absolute'});
+})
+
+
+
+$(".sect4").on('mouseover', function(){
+	$(".table4").css({top:387, left: 208, position:'absolute'});
+})
+$(".sect4").on('mouseout', function(){
+	$(".table4").css({top:405, left: 208, position:'absolute'});
+})
+
+
+
+$(".sect5").on('mouseover', function(){
+	$(".table5").css({top:507, left: 208, position:'absolute'});
+})
+$(".sect5").on('mouseout', function(){
+	$(".table5").css({top:525, left: 208, position:'absolute'});
+})
+
+
+
+$(".sect6").on('mouseover', function(){
+	$(".table6").css({top:627, left: 208, position:'absolute'});
+})
+$(".sect6").on('mouseout', function(){
+	$(".table6").css({top:645, left: 208, position:'absolute'});
+})
+
+
+
+});
